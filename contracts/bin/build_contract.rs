ce828654a94aab342fa01614b21d3fde3cbed1ea
Furkan Ahmet Kara <furkanahmetkara.fk@gmail.com>
2026-01-11 17:21:32 +0300

    Update contracts to fix invalid arg error
    Signed-off-by: Furkan Ahmet Kara <furkanahmetkara.fk@gmail.com>

--- contracts/bin/build_contract.rs
+++ contracts/bin/build_contract.rs
@@ -1,5 +1,18 @@
 #![doc = "Binary for building wasm files from odra contracts."]
 #![no_std]
-#![no_main]
+#![cfg_attr(target_arch = "wasm32", no_main)]
 #![allow(unused_imports, clippy::single_component_path_imports)]
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {}
+
+#[cfg(target_arch = "wasm32")]
+extern crate odra_casper_wasm_env;
+
+#[cfg(target_arch = "wasm32")]
+use shroud_protocol::shroud_protocol::ShroudProtocol;
+
+#[cfg(target_arch = "wasm32")]
+use odra_casper_wasm_env as _;
+
 use shroud_protocol;
